f7f810c13a766eab458c9f272077194be4d0e35f
Yulissa <yulissa.aguila.2003@gmail.com>
2024-12-16 13:57:11 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names,event){
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    }
+    return messages;
+}
+
+function countDown(n){
+    let i = n;
+    while (i >= 0){
+        console.log(i);
+        i--;
+    }
+}
